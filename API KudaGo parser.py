3d0a690302d16c750baba558074adb1f670aca5f
fedix <d.fedor97@gmail.com>
2020-03-30 14:52:10 +0300

    fixes

--- API KudaGo parser.py
+++ API KudaGo parser.py
@@ -1,49 +1,28 @@
-# https://kudago.com/public-api/v1.4
-
+import json
+import requests
 
 """Parser API KudaGo. Full list of places in Saint Petersburg."""
 """Input: site`s API url. Output: .json file with class 'list':
     [{'id':...,'name':...,'categories':[...]},...]"""
 
-from urllib.request import urlopen
-from urllib.error import HTTPError
-from bs4 import BeautifulSoup
-import json
-
 url_base = "https://kudago.com/public-api/v1.4/places/?"
-url_fields = "&fields=id,title,categories"
+url_fields = "&fields=id,title,categories,coords"
 url_location = "&location=spb"
-url_pages = "&page_size=100&page="
+url_pages = "&page_size=100"
 url_full = url_base + url_fields + url_location + url_pages
-page = 1
-max_page_spb = 39  # Found manually for: location=spb, page_size=100.
 
+data = []
+with open('data/Places_spb_list.json', 'w', encoding='utf-8') as ouf:
+    while url_full is not None:
+        response = requests.get(url_full)
+        page_data = response.json()
+        data.extend(page_data['results'])
+        url_full = page_data['next']
+    print(f'parsed {len(data)} places')
 
-def get_data(url):
-    try:
-        html = urlopen(url)
-    except HTTPError as e:
-        return e
-    try:
-        data = str(BeautifulSoup(html.read()))
-    except AttributeError as e:
-        return None
-    return data
+    # delete places with no categories
+    data = list(filter(lambda place: len(place['categories']) > 0, data))
 
-
-page_data = ''
-data = ''
-with open('data/Places_spb_list.json',
-          'w', encoding="utf-8") as ouf:
-    while (page_data != None):
-        page_data = get_data(url_full + str(page))
-        if (type(page_data) != str):
-            break
-        if page == 1:
-            data = json.loads(page_data[page_data.find('['):-1:1])
-        else:
-            data += json.loads(page_data[page_data.find('['):-1:1])
-        page += 1
     category_sorted = sorted(data, key=lambda x: x['categories'][0])
-    json.dump(category_sorted, ouf)
-    print(page)
+    print(f'dumping {len(data)} places to json')
+    json.dump(category_sorted, ouf, indent=2, ensure_ascii=False)
